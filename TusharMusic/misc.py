f7377a08b3cedef22f3ff2bc22242a85d40722c6
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 02:46:36 +0530

    Update misc.py

--- TusharMusic/misc.py
+++ TusharMusic/misc.py
@@ -5,7 +5,7 @@ import heroku3
 from pyrogram import filters
 
 import config
-from NetflixMusic.core.mongo import mongodb
+from TusharMusic.core.mongo import mongodb
 
 from .logging import LOGGER
 
