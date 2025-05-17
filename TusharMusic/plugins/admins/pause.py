2f91b98e6e40472589a5dfc3e29503e1c55de00a
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 18:40:29 +0530

    Update pause.py

--- TusharMusic/plugins/admins/pause.py
+++ TusharMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from NetflixMusic import app
-from NetflixMusic.core.call import Netflix
-from NetflixMusic.utils.database import is_music_playing, music_off
-from NetflixMusic.utils.decorators import AdminRightsCheck
-from NetflixMusic.utils.inline import close_markup
+from TusharMusic import app
+from TusharMusic.core.call import Tushar
+from TusharMusic.utils.database import is_music_playing, music_off
+from TusharMusic.utils.decorators import AdminRightsCheck
+from TusharMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Netflix.pause_stream(chat_id)
+    await Tushar.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
