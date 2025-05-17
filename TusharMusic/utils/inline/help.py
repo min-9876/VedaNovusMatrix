0b9c52a6805e2ad9c90dff023c0dd6f0b0da5b4e
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 14:41:47 +0530

    Update help.py

--- TusharMusic/utils/inline/help.py
+++ TusharMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from NetflixMusic import app
+from TusharMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
