5c01fe9bd23b55ae97eb74a99f4ba05eaaf41948
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 10:03:26 +0530

    Update bot.py

--- TusharMusic/core/bot.py
+++ TusharMusic/core/bot.py
@@ -9,11 +9,11 @@ import config
 from ..logging import LOGGER
 
 
-class Netflix(Client):
+class Tushar(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="NetflixMusic",
+            name="TusharMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -32,7 +32,7 @@ class Netflix(Client):
         try:
             await self.send_message(
                 chat_id=config.LOG_GROUP_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<u><b>» {self.mention} Bot started :</b><u>\n\nID : <code>{self.id}</code>\nName : {self.name}\nUsername : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
