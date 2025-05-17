ed589eea89a62f7476d0c8f477a2874fea6b8682
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 14:42:21 +0530

    Update play.py

--- TusharMusic/utils/inline/play.py
+++ TusharMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from NetflixMusic.utils.formatters import time_to_seconds
+from TusharMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
