e6030feabeb4b022389ae047de8526025f3f4ac8
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 18:50:17 +0530

    Update watcher.py

--- TusharMusic/plugins/misc/watcher.py
+++ TusharMusic/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from NetflixMusic import app
-from NetflixMusic.core.call import Netflix
+from TusharMusic import app
+from TusharMusic.core.call import Tushar
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Netflix.stop_stream_force(message.chat.id)
+    await Tushar.stop_stream_force(message.chat.id)
