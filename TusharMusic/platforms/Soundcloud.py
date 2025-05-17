cdf6be499837738cdfa93702c319ba9c490c4d9d
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 13:46:58 +0530

    Update Soundcloud.py

--- TusharMusic/platforms/Soundcloud.py
+++ TusharMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from NetflixMusic.utils.formatters import seconds_to_min
+from TusharMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
