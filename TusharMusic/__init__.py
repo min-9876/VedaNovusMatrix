75a2f03856fff8d084d15e2a5db977d54f94c23a
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 13:51:49 +0530

    Update __init__.py

--- TusharMusic/__init__.py
+++ TusharMusic/__init__.py
@@ -17,6 +17,10 @@ userbot = Userbot()
 
 from .platforms import *
 
-
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
