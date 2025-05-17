63c1383db3afd55b53be3f128b53f30484838ef5
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 10:07:35 +0530

    Update userbot.py

--- TusharMusic/core/userbot.py
+++ TusharMusic/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="NetflixAss1",
+            name="TusharAssistant1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="NetflixAss2",
+            name="TusharAssistant2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="NetflixAss3",
+            name="TusharAssistant3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="NetflixAss4",
+            name="TusharAssistant4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="NetflixAss5",
+            name="TusharAssistant5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
