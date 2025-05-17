66a66aeb37541b7113ade8648821b7340542e2e7
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 18:06:29 +0530

    Update stream.py

--- TusharMusic/utils/stream/stream.py
+++ TusharMusic/utils/stream/stream.py
@@ -11,7 +11,7 @@ from TusharMusic.misc import db
 from TusharMusic.utils.database import add_active_video_chat, is_active_chat
 from TusharMusic.utils.exceptions import AssistantErr
 from TusharMusic.utils.inline import aq_markup, close_markup, stream_markup
-from TusharMusic.utils.pastebin import NetflixBin
+from TusharMusic.utils.pastebin import TusharBin
 from TusharMusic.utils.stream.queue import put_queue, put_queue_index
 from TusharMusic.utils.thumbnails import gen_thumb
 
