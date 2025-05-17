1d5ec878d7c3bc473cacf367f2a5570a7083f388
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-17 13:39:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Crush2161/Crushbackup",
+    "https://github.com/SudarshanTushar/VedaNovusMatrix",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
