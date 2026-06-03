fa18c5b6f9a1c76bb5ae56e0905e929adc529258
min-9876 <musicmin2029@gmail.com>
2026-06-03 15:25:25 +0630

    Update __init__.py

--- TusharMusic/__init__.py
+++ TusharMusic/__init__.py
@@ -1,3 +1,10 @@
+import asyncio
+import uvloop
+
+# Event Loop ကို အရင်ဆုံး စတင်သတ်မှတ်ပေးပါ
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+# ပြီးမှ ကျန်တဲ့ Import များကို ဆက်ရေးပါ
 from TusharMusic.core.bot import Tushar
 from TusharMusic.core.dir import dirr
 from TusharMusic.core.git import git
@@ -13,7 +20,6 @@ heroku()
 
 app = Tushar()
 userbot = Userbot()
-
 
 from .platforms import *
 
